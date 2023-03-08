69b4044cd0d4a486afde90e4044c1043973f2a4d
Zakir.Mohammad <zmohammad@schoology.com>
2023-03-08 16:17:45 +0530

    dependency injection

--- src/User.php
+++ src/User.php
@@ -1,11 +1,29 @@
 <?php
 class User
 {
+    /** @var string */
     public $first_name;
+    /** @var string */
     public $last_name;
+    /** @var string */
+    public $email;
+    /** @var Mailer */
+    public $mailer;
+
+    /**
+     * @param Mailer $mailer
+     */
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
 
     public function getFullName(): string
     {
         return trim($this->first_name. " ". $this->last_name);
+    }
+
+    public function notify($message) {
+        return $this->mailer->sendEmail($this->email, $message);
     }
 }
